e61f4c7d9027d92cfeddb98ff1457a371f983529
mastereljay <normanlj77@hotmail.com>
2020-01-28 12:17:57 -0800

    adding project folders

---
+++ STOCK FILES/script.js
@@ -0,0 +1,5 @@
+//Stock JavaScript File to get you going on a project.
+
+var userName = prompt ("THE JavaScript is Working!");
+alert ("Nice to meet you, " + userName);
+console.log("Also great to meet you, " + userName);
